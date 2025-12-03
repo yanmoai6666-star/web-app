4dc0a6147b36fbf79c0f49d9d54f20ba16223ac8
yanmoai6666-star <yanmoai6667@163.com>
2025-12-03 20:10:48 +0800

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = ({ title }) => {
+  return (
+    <header style={{
+      background: '#282c34',
+      padding: '20px',
+      color: white,
+      textAlign: 'center'
+    }}>
+      <h1>{title}</h1>
+    </header>
+  );
+};
+
+export default Header;
